5b016578dd5ab9b3382ce67c1f2fc50eb43a6ac6
Wieland Hilker <60106690+AlphaMoonbaseBerlin@users.noreply.github.com>
2025-10-09 15:13:58 +0200

    added default value to .packagefolder env definition.

--- AppData/Scripts/sys.py
+++ AppData/Scripts/sys.py
@@ -7,12 +7,14 @@ import sys as __surely_not_even_close_to_sys
 def _setup_path_from_packagefolder():
     import os, re
     from sys import path
+
     def replace_var(match):
         var_name = match.group(1)
-        return os.environ[var_name]  # Fallback to original if not found
+        if len( env_naming := var_name.split("||") ) == 2:
+            return os.environ.get( env_naming[0], env_naming[1] )
+        else:
+            return os.environ[env_naming[0]]
 
-    # Regex to find ${...} and replace using the dictionary
-    
     with open(".packagefolder", "a+t") as package_folder_file:
         # Lets read the .packagefolder and add specified elements. 
         # Monkeybrain uv run mb init.files will create this, otherwise it will simply be empty.
@@ -22,11 +24,10 @@ def _setup_path_from_packagefolder():
             if not line: continue # skip empty lines
             if line.startswith("#"): continue # skip comments
             try:
-                # fetch env, skip if env variable is not defined.
                 enved_line = re.sub(r"\$\{([^}]+)\}", replace_var, line)
             except KeyError:
                 continue
-            # if already in there skip too.
+
             if enved_line in path: continue
 
             # ok, now insert.
